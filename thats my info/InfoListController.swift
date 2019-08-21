cb2068dd3c3b0e0e1ae2d8ad32e863a141ac57ef
corykim0829 <corykim0829@gmail.com>
2019-08-22 03:34:22 +0900

    #4 feat: just add all posts

--- thats my info/InfoListController.swift
+++ thats my info/InfoListController.swift
@@ -9,72 +9,60 @@
 import LBTATools
 import Alamofire
 
-struct InfoCategory {
+struct InfoTitleUrl {
     let title: String
     let url: String
 }
 
-class InfoCell: LBTAListCell<InfoCategory> {
+struct InfoPost {
+    let title: String
+    let url: String
+    var infoPostDataDict: Dictionary<String, AnyObject>?
+}
+
+class InfoCell: LBTAListCell<InfoPost> {
     
     var infoDictsArray: [Dictionary<String, AnyObject>] = []
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.textColor = .darkGray
+        label.textColor = .white
         label.backgroundColor = .clear
-        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.font = .systemFont(ofSize: 20, weight: .bold)
         return label
     }()
     
-    var titleContainer: UIView = {
-        let view = UIView(backgroundColor: .red)
-        view.layer.cornerRadius = 16
+    var containerView: UIView = {
+        let view = UIView(backgroundColor: .purple)
+        view.layer.cornerRadius = 8
         return view
     }()
     
-    var infoLabel = UILabel(text: "0", font: .systemFont(ofSize: 18, weight: .bold), textColor: .darkGray, textAlignment: .left)
-    
-    override var item: InfoCategory! {
+    override var item: InfoPost! {
         didSet {
             titleLabel.text = item.title
-            
-            let requestURL = "https://rs-privacy.azurewebsites.net/info\(item.url)"
-            Alamofire.request(requestURL).responseJSON(completionHandler: { (response) in
-                let result = response.result
-                
-                if let array = result.value as? [AnyObject] {
-                    array.forEach({ (value) in
-                        if let dict = value as? Dictionary<String, AnyObject> {
-                            self.infoDictsArray.append(dict)
-                        }
-                    })
-                } else if let dict = result.value as? Dictionary<String, AnyObject> {
-                    guard self.infoDictsArray.isEmpty else { return }
-                    self.infoDictsArray.append(dict)
-                }
-                print(self.item.url, self.infoDictsArray.count)
-                self.infoLabel.text = "\(self.item.url), \(self.infoDictsArray.count)"
-            })
         }
     }
     
     override func setupViews() {
         super.setupViews()
         
+        setupUI()
+    }
+    
+    fileprivate func setupUI() {
         backgroundColor = .white
         
-        addSubview(titleLabel)
-        titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 8, left: 16, bottom: 0, right: 0))
+        addSubview(containerView)
+        containerView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 8, bottom: 0, right: 8))
         
-        addSubview(titleContainer)
-        titleContainer.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 8, left: 16, bottom: 0, right: 16), size: .init(width: 0, height: 88))
-        
-        titleContainer.addSubview(infoLabel)
-        infoLabel.centerInSuperview()
+        containerView.addSubview(titleLabel)
+        titleLabel.anchor(top: containerView.topAnchor, leading: containerView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 0, left: 16, bottom: 0, right: 0))
+        titleLabel.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
     }
 }
 
-class InfoListController: LBTAListController<InfoCell, InfoCategory>, UICollectionViewDelegateFlowLayout {
+class InfoListController: LBTAListController<InfoCell, InfoPost>, UICollectionViewDelegateFlowLayout {
     
     fileprivate let topToSafeAreaView = UIView(backgroundColor: #colorLiteral(red: 0.1333333333, green: 0.6941176471, blue: 0.9647058824, alpha: 1))
     
@@ -85,42 +73,51 @@ class InfoListController: LBTAListController<InfoCell, InfoCategory>, UICollecti
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        items = [
-            .init(title: "개인정보란?", url: "/privacy-is"),
-            .init(title: "개인정보 보호 관련 법제", url: "/rules"),
-            .init(title: "개인정보 보호 이용수칙", url: "/rules-of-use"),
-            .init(title: "내 정보 지킴이 캠페인", url: "/campaign")
-        ]
-        
-//        fetchDatas()
+//        items = [
+//            .init(title: "개인정보란?", url: "/privacy-is"),
+//            .init(title: "개인정보 보호 관련 법제", url: "/rules"),
+//            .init(title: "개인정보 보호 이용수칙", url: "/rules-of-use"),
+//            .init(title: "내 정보 지킴이 캠페인", url: "/campaign")
+//        ]
         setupUI()
         // bug
-//        setupTapGesture()
+        setupTapGesture()
         infoNavBar.backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
+        
+        fetchDatas()
     }
     
     fileprivate func fetchDatas() {
+        let infoDatas: [InfoTitleUrl] = [
+            InfoTitleUrl(title: "개인정보란?", url: "/privacy-is"),
+            InfoTitleUrl(title: "개인정보 보호 관련 법제", url: "/rules"),
+            InfoTitleUrl(title: "개인정보 보호 이용수칙", url: "/rules-of-use"),
+            InfoTitleUrl(title: "내 정보 지킴이 캠페인", url: "/campaign")
+        ]
         
-        items.forEach { (item) in
-            let requestURL = "https://rs-privacy.azurewebsites.net/info\(item.url)"
+//        let urls = ["/privacy-is", "/rules", "/rules-of-use", "/campaign"]
+        
+        infoDatas.forEach { (infoTitleUrl) in
             
+            var infoDictsArray: [Dictionary<String, AnyObject>] = []
+            
+            let requestURL = "https://rs-privacy.azurewebsites.net/info\(infoTitleUrl.url)"
             Alamofire.request(requestURL).responseJSON(completionHandler: { (response) in
-                var infoDictsArray: [Dictionary<String, AnyObject>] = []
                 let result = response.result
-//                print(result.value)
                 
                 if let array = result.value as? [AnyObject] {
                     array.forEach({ (value) in
                         if let dict = value as? Dictionary<String, AnyObject> {
                             infoDictsArray.append(dict)
-                            print(item.title, infoDictsArray.count)
+                            self.items.append(.init(title: dict["title"] as? String ?? "", url: infoTitleUrl.url, infoPostDataDict: dict))
                         }
                     })
                 } else if let dict = result.value as? Dictionary<String, AnyObject> {
+                    guard infoDictsArray.isEmpty else { return }
                     infoDictsArray.append(dict)
-//                    print(infoDictsArray)
-                    print(item.title, infoDictsArray.count)
+                    self.items.append(.init(title: infoTitleUrl.title, url: infoTitleUrl.url, infoPostDataDict: dict))
                 }
+//                print(infoTitleUrl, infoDictsArray.count)
             })
         }
     }
@@ -139,9 +136,9 @@ class InfoListController: LBTAListController<InfoCell, InfoCategory>, UICollecti
     
     fileprivate func setupUI() {
 //        collectionView.alwaysBounceVertical = true
-//        collectionView.isScrollEnabled = false
+        collectionView.isScrollEnabled = false
         collectionView.backgroundColor = .white
-        collectionView.contentInset.top = navBarHeight + 16
+        collectionView.contentInset.top = navBarHeight + 8
         
         collectionView.addSubview(infoNavBar)
         collectionView.addSubview(topToSafeAreaView)
@@ -151,28 +148,16 @@ class InfoListController: LBTAListController<InfoCell, InfoCategory>, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 8
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let estimatedSizeCell = InfoCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 400))
-        
-        estimatedSizeCell.item = self.items[indexPath.item]
-        
-        estimatedSizeCell.layoutIfNeeded()
-        
-        let estimatedSize = estimatedSizeCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 400))
-        
-        return .init(width: view.frame.width, height: estimatedSize.height)
-        
-//        return .init(width: collectionView.frame.width, height: cellHeight)
+        return .init(width: view.frame.width, height: 56)
     }
     
-//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let infoCategory = items[indexPath.item]
-//        let vc = UIViewController()
-//        vc.view.backgroundColor = .red
-//        navigationController?.pushViewController(vc, animated: true)
-//    }
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let infoCategory = items[indexPath.item]
+        
+    }
 }
