cf23918e5a49555593ce7eb18c149378a551698f
corykim0829 <corykim0829@gmail.com>
2019-09-03 23:33:05 +0900

    update: README

--- README.md
+++ README.md
@@ -1,34 +1,63 @@
-## That's My Info!
+## 님아 그것은 내정보
 
 ![swift](<https://img.shields.io/badge/swift-5.0-orange.svg>) ![platform](<https://img.shields.io/badge/platform-iOS-9cf.svg>) ![LBTATools](<https://img.shields.io/badge/LBTATools-blue.svg>) ![Alamofire](<https://img.shields.io/badge/Alamofire-red.svg>) ![naver](<https://img.shields.io/badge/Naver Login Auth-green.svg>)
 
-내 개인정보는 내가 지킨다.
-
-웹상의 개인정보를 크롤링하여 링크와 화면을 제공, 조치나 신고를 할 수 있음.
-
-생활간편 서비스
+내 개인정보는 내가 지킨다!
 
 
 
-개발자 4명
+### Key Features
 
-iOS
+- 웹상의 노출된 개인정보 탐색 및 삭제
+  - 본인 인증 후 주요 포털에 노출된 개인정보 탐색
+  - 개인정보 삭제 요청 또는 신고 가능
 
-서버
+- 개인정보 보호 안내
+  - 개인정보 포털에서 제공되는 법령 해석
+  - 분쟁 조정 사례 및 관련 기사 제공
+  - 키워드를 통한 관련 법 및 사례 검색
+- 개인정보 침해 신고
+  - 온/오프라인에서의 개인정보 관련 분쟁조정 신청 및 침해 신고 지원
+
+
+
+### Team
+
+- iOS - 1
+
+- Back-end - 3
 
 
 
 ### 디자인 패턴
 
-MVVM을 사용하려고 했지만
-
-하나의 컨트롤러를 제외하고는 MVC 패턴과 유사하게 만들어졌다.
+MVVM
 
 
 
 ### git flow
 
 gitHub의 issue, project를 사용한 업무처리
+
+
+
+### Screenshots
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen1.png" width="200px" />
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen2.png" width="200px" />
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen3.png" width="200px" />
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen4.png" width="200px" />
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen5.png" width="200px" />
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen6.png" width="200px" />
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen7.png" width="200px" />
+
+<img src="https://github.com/corykim0829/thats-my-info/tree/master/screenshots/screen8.png" width="200px" />
 
 
 
---
+++ thats my info/ReportStackView 2.swift
@@ -0,0 +1,89 @@
+//
+//  ReportStackView.swift
+//  thats my info
+//
+//  Created by Cory Kim on 15/08/2019.
+//  Copyright © 2019 Relationship oriented. All rights reserved.
+//
+
+import UIKit
+import LBTATools
+
+class ReportStackView: UIStackView {
+    
+    let titleFontSize: CGFloat = 18
+    let buttonsLabelColor: UIColor = .white
+    
+    fileprivate func createButton() -> UIButton {
+        let button = UIButton(type: .system)
+        button.backgroundColor = #colorLiteral(red: 0.1333333333, green: 0.5889699587, blue: 0.9647058824, alpha: 1)
+        button.setupShadow(opacity: 0.3, radius: 8, offset: .init(width: 2, height: 2), color: .gray)
+        button.layer.cornerRadius = 32
+        return button
+    }
+    
+    lazy var infringementButtonlabel = UILabel(text: "개인정보\n침해신고", font: .systemFont(ofSize: titleFontSize, weight: .bold), textColor: buttonsLabelColor, textAlignment: .left, numberOfLines: 2)
+    lazy var leakButtonlabel = UILabel(text: "개인정보\n유출신고", font: .systemFont(ofSize: titleFontSize, weight: .bold), textColor: buttonsLabelColor, textAlignment: .left, numberOfLines: 2)
+    lazy var feudButtonlabel = UILabel(text: "개인정보\n분쟁신고", font: .systemFont(ofSize: titleFontSize, weight: .bold), textColor: buttonsLabelColor, textAlignment: .left, numberOfLines: 2)
+    lazy var collectionButtonlabel = UILabel(text: "개인정보\n수집신고", font: .systemFont(ofSize: titleFontSize, weight: .bold), textColor: buttonsLabelColor, textAlignment: .left, numberOfLines: 2)
+    
+    lazy var infringementButton = createButton()
+    lazy var leakButton = createButton()
+    lazy var feudButton = createButton()
+    lazy var collectionButton = createButton()
+    
+    lazy var buttonsStackView1: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [
+            infringementButton,
+            leakButton
+            ])
+        sv.axis = .horizontal
+        sv.distribution = .fillEqually
+        sv.spacing = 16
+        return sv
+    }()
+    
+    lazy var buttonsStackView2: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [
+            feudButton,
+            collectionButton
+            ])
+        sv.axis = .horizontal
+        sv.distribution = .fillEqually
+        sv.spacing = 16
+        return sv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupStackView()
+    }
+    
+    fileprivate func setupStackView() {
+        addArrangedSubview(buttonsStackView1)
+        addArrangedSubview(buttonsStackView2)
+        
+        axis = .vertical
+        distribution = .fillEqually
+        spacing = 16
+    }
+    
+    fileprivate func setupUI() {
+        backgroundColor = .clear
+        
+        let leftPaddingValue:CGFloat = 20
+        infringementButton.addSubview(infringementButtonlabel)
+        infringementButtonlabel.anchor(top: infringementButton.topAnchor, leading: infringementButton.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 26, left: leftPaddingValue, bottom: 0, right: 0))
+        leakButton.addSubview(leakButtonlabel)
+        leakButtonlabel.anchor(top: leakButton.topAnchor, leading: leakButton.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 26, left: leftPaddingValue, bottom: 0, right: 0))
+        feudButton.addSubview(feudButtonlabel)
+        feudButtonlabel.anchor(top: feudButton.topAnchor, leading: feudButton.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 26, left: leftPaddingValue, bottom: 0, right: 0))
+        collectionButton.addSubview(collectionButtonlabel)
+        collectionButtonlabel.anchor(top: collectionButton.topAnchor, leading: collectionButton.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 26, left: leftPaddingValue, bottom: 0, right: 0))
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
